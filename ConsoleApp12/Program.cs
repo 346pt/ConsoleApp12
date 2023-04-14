e97822971b0657e5887117e5934863636ebc35af
Антисептик <@DESKTOP-KTN6EI8>
2023-04-14 13:02:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,6 @@
+﻿StreamReader text = new StreamReader(@"C:\Users\Антисептик\Desktop\лаба11(10)");
+string[] massiv = text.ReadToEnd().Split('\n');
+foreach (var i in massiv)
+{
+    Console.WriteLine(i);
+}
